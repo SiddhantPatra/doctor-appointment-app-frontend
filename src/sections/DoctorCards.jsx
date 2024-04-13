41b848e8147e6546f090ef1810dcfd9b84ccefce
Siddhant patra <siddhant.patra2010@gmail.com>
2024-04-13 18:30:38 +0530

    Link too doctor cards

--- src/sections/DoctorCards.jsx
+++ src/sections/DoctorCards.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import { doctors } from '../constants'
+import React from "react";
+import { doctors } from "../constants";
 import DoctorCard from "../components/DoctorCard";
+import { Link } from "react-router-dom";
 const DoctorCards = () => {
   return (
     <section className="lg:my-4 container">
-       
       <div className=" grid grid-cols-1 lg:grid-cols-4 gap-5">
-        {doctors.map((team) =>(<DoctorCard key={team.imgURL} {...team}/>))}
+        {doctors.map((team) => (
+          <Link to={"/doctor"}>
+            <DoctorCard key={team.imgURL} {...team} />
+          </Link>
+        ))}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default DoctorCards
+export default DoctorCards;
