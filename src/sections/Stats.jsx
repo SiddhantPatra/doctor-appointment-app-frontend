904cbceceba291f84bf39bf8268385e0f2f9decb
Siddhant patra <siddhant.patra2010@gmail.com>
2024-04-18 23:06:41 +0530

    counter widget added

--- src/sections/Stats.jsx
+++ src/sections/Stats.jsx
@@ -1,12 +1,13 @@
  
-
+import CountUp from 'react-countup';
 const Stats = () => {
   return (
     <section className="max-sm:hidden">
       <h1 className="text-center text-teal-600 text-3xl">Our results in numbers</h1>
       <div className="flex flex-1 justify-center gap-20 py-10 mb-4 mt-4 ">
         <div className="block text-center">
-          <h1 className="text-green-600 text-4xl">99%</h1>
+          <h1 className="text-green-600 text-4xl">
+          <CountUp end={99} />%</h1>
           <p>Customer satisfaction</p>
         </div>
         <div className="block text-center">
